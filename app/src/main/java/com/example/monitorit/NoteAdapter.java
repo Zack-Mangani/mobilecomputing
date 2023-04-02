910228ee1216bb3458de85f5052e5071ec83a733
Zack-Mangani <zack.mangani.21@um.edu.mt>
2023-04-03 00:39:41 +0200

    Continued working on the app

--- app/src/main/java/com/example/monitorit/NoteAdapter.java
+++ app/src/main/java/com/example/monitorit/NoteAdapter.java
@@ -1,4 +1,5 @@
 package com.example.monitorit;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,32 +10,31 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
+
 
 public class NoteAdapter extends ArrayAdapter<Note> {
 
-    public NoteAdapter(Context context, List<Note> notes) {
-        super(context, 0, notes);
+    public NoteAdapter(Context context, List<Note> notes){
+        super(context,0,notes);
 
     }
-
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        Note note = getItem(position);
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.athletenote_cell, parent, false);
-        }
-            TextView athlete = convertView.findViewById(R.id.cellAthlete);
-            TextView desc = convertView.findViewById(R.id.cellDesc);
+        Note note  = getItem(position);
+                if(convertView == null)
+                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.note_cell,parent,false);
 
-            athlete.setText(note.getName());
-            desc.setText(note.getDescription());
+        TextView title = convertView.findViewById(R.id.cellTitle);
+        TextView desc = convertView.findViewById(R.id.cellDesc);
+
+        title.setText(note.getTitle());
+        desc.setText(note.getDescription());
+
+        return convertView;
 
 
-            return convertView;
-        }
     }
-
+}
